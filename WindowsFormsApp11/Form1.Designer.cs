06201b154608b1a47427c11872e03680803004ea
DeStiNoVich21 <122779819+DeStiNoVich21@users.noreply.github.com>
2023-02-06 21:04:17 +0600

    Добавьте файлы проекта.

---
+++ WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.extendedTreeView1 = new EWSoftware.ListControls.ExtendedTreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.idToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.supportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ColorModChange = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox1.Location = new System.Drawing.Point(267, 33);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1040, 571);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = " ";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // extendedTreeView1
+            // 
+            this.extendedTreeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.extendedTreeView1.BackColor = System.Drawing.Color.White;
+            this.extendedTreeView1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.extendedTreeView1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.extendedTreeView1.ForeColor = System.Drawing.Color.Black;
+            this.extendedTreeView1.LineColor = System.Drawing.Color.DimGray;
+            this.extendedTreeView1.Location = new System.Drawing.Point(0, 33);
+            this.extendedTreeView1.Name = "extendedTreeView1";
+            this.extendedTreeView1.Size = new System.Drawing.Size(261, 571);
+            this.extendedTreeView1.TabIndex = 3;
+            this.extendedTreeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.extendedTreeView1_AfterSelect);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.idToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1319, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.openFolderToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(156, 24);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(156, 24);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(156, 24);
+            this.saveAsToolStripMenuItem.Text = "Save as";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(156, 24);
+            this.openFolderToolStripMenuItem.Text = "OpenFolder";
+            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.openFolderToolStripMenuItem_Click);
+            // 
+            // idToolStripMenuItem
+            // 
+            this.idToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.supportToolStripMenuItem,
+            this.ColorModChange});
+            this.idToolStripMenuItem.Name = "idToolStripMenuItem";
+            this.idToolStripMenuItem.Size = new System.Drawing.Size(34, 24);
+            this.idToolStripMenuItem.Text = "Id";
+            // 
+            // supportToolStripMenuItem
+            // 
+            this.supportToolStripMenuItem.Name = "supportToolStripMenuItem";
+            this.supportToolStripMenuItem.Size = new System.Drawing.Size(146, 24);
+            this.supportToolStripMenuItem.Text = "Support";
+            this.supportToolStripMenuItem.Click += new System.EventHandler(this.supportToolStripMenuItem_Click);
+            // 
+            // ColorModChange
+            // 
+            this.ColorModChange.Name = "ColorModChange";
+            this.ColorModChange.Size = new System.Drawing.Size(146, 24);
+            this.ColorModChange.Text = "Light mod";
+            this.ColorModChange.Click += new System.EventHandler(this.lightModeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(18)))), ((int)(((byte)(57)))));
+            this.ClientSize = new System.Drawing.Size(1319, 609);
+            this.Controls.Add(this.extendedTreeView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Text redactor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private EWSoftware.ListControls.ExtendedTreeView extendedTreeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem idToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ToolStripMenuItem supportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ColorModChange;
+    }
+}
---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.WindowsAPICodePack.Dialogs;
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        // path of the chosen folder
+        private string path = @"C:/Users/User/source/repos/WindowsFormsApp11/WindowsFormsApp11/Documents/";
+        private int singleorfolder = 0;
+        // value nneded to correctly change the colormod
+        private int f = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                DirectoryInfo d = new DirectoryInfo(path); //Assuming Test is your Folder
+
+                FileInfo[] Files = d.GetFiles("*.txt"); //Getting Text files
+               
+                    foreach (FileInfo file in Files)
+                    {
+
+                    if (file.Exists)
+                    {
+                        extendedTreeView1.Nodes.Add(file.Name);
+                    }
+                       
+                    }
+         
+            } 
+            catch(IOException ex)
+            {
+                MessageBox.Show(ex.ToString(),"Error",MessageBoxButtons.RetryCancel, MessageBoxIcon.Error) ;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private async void guna2Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void extendedTreeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            
+            try
+            {
+                if (singleorfolder == 1)
+                {
+                        // opening the chosen txt file
+                    richTextBox1.Text = "";
+                    richTextBox1.Text = File.ReadAllText(extendedTreeView1.SelectedNode.Text);               
+                }
+                else
+                {
+                    // opening the chosen txt file
+                    richTextBox1.Text = "";
+                    richTextBox1.Text = File.ReadAllText(path + "/" + extendedTreeView1.SelectedNode.Text);
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+
+        }
+        private void saver()
+        {
+
+        }
+        private async void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                Stream myStream;
+                SaveFileDialog savas = new SaveFileDialog();
+                savas.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                savas.FilterIndex = 2;
+                savas.RestoreDirectory = true;
+
+                if (savas.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter writer = new StreamWriter(savas.FileName.ToString(), false))
+                    { 
+                        await writer.WriteLineAsync(richTextBox1.Text);
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void openFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (singleorfolder ==1)
+            { 
+            //singleorfolder = singleorfolder * 0;
+                extendedTreeView1.Nodes.Clear();
+            }
+            try
+            {
+                // opening file dialog
+                CommonOpenFileDialog opg = new CommonOpenFileDialog();
+                opg.IsFolderPicker = true;
+                opg.ShowDialog();
+              
+
+                DirectoryInfo d = new DirectoryInfo(opg.FileName.ToString()); //Assuming Test is your Folder
+
+                FileInfo[] Files = d.GetFiles(); //Getting Text files
+                path = opg.FileName.ToString();
+                //clearing and re-adding the treeview
+                extendedTreeView1.Nodes.Clear();
+                foreach (FileInfo file in Files)
+                {
+
+                    if (file.Exists)
+                    { 
+
+                        extendedTreeView1.Nodes.Add(file.Name);
+                    }
+
+                }
+
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            // functionality of working with separate files
+            if (singleorfolder == 0)
+            {
+                singleorfolder = singleorfolder+ 1;
+                extendedTreeView1.Nodes.Clear();
+            }
+            try
+            {
+                // opening the chosen txt file
+
+                richTextBox1.Text = "";
+                CommonOpenFileDialog com = new CommonOpenFileDialog();
+                com.ShowDialog();
+                extendedTreeView1.Nodes.Add(com.FileName.ToString());
+                richTextBox1.Text = File.ReadAllText(com.FileName.ToString());
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void guna2Button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private async void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // works if the only separete element is opened
+                if (singleorfolder == 1)
+                {
+                    string pathe = extendedTreeView1.SelectedNode.Text;
+                    // полная перезапись файла 
+                    if (File.Exists(pathe))
+                    {
+
+                        using (StreamWriter writer = new StreamWriter(pathe, false))
+                        {
+
+                            await writer.WriteLineAsync(richTextBox1.Text);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("The File doesnt exist", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+
+                }
+                // Works if you work with folder
+                else
+                {
+
+                    string pathe = path + "/" + extendedTreeView1.SelectedNode.Text;
+                    // saving already existing file  by REwriting it
+                    if (File.Exists(pathe))
+                    {
+
+                        using (StreamWriter writer = new StreamWriter(pathe, false))
+                        {
+
+                            await writer.WriteLineAsync(richTextBox1.Text);
+                        }
+                        MessageBox.Show("Successfully saved", "Success");
+                    }
+                    else
+                    {
+                        MessageBox.Show("The File doesnt exist", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+        }
+
+        private void fontSizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+         
+        }
+
+        private void lightModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            if (f == 0)
+            {
+                richTextBox1.BackColor = Color.Black;
+                richTextBox1.ForeColor = Color.White;
+                extendedTreeView1.ForeColor = Color.White;
+                extendedTreeView1.BackColor = Color.Black;
+                ColorModChange.Text = "Light Mod";
+                f = 1;
+            }
+            else
+            {
+                richTextBox1.BackColor = Color.White;
+                richTextBox1.ForeColor = Color.Black;
+                extendedTreeView1.ForeColor = Color.Black;
+                extendedTreeView1.BackColor = Color.White;
+                ColorModChange.Text = "Black Mod";
+                f = 0;
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void supportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I really appreciate that you like my app", "Thank you");
+        }
+    }
+}
